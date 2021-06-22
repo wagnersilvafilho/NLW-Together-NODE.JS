3fce26ef00c09a2ff13fdd0994e6897fd7fd2827
wagnersilvafilho <wagnersf97@gmail.com>
2021-06-22 10:51:57 -0300

    Final treinamento primeiro dia (GET & POST)

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,25 @@
-import express from "express";
+import express, { request, response } from "express";
 
 // @types/express
 const app = express();
 
+/**
+ * GET    => Buscar uma informação
+ * POST   => Inserir (criar) uma informação
+ * PUT    => Alterar uma informação
+ * DELETE => Remover um dado
+ * PATCH  => Alterar uma informação específica
+ */
+
+app.get("/test", (request, response) => {
+    // Request => Entrando
+    // Response => Saindo
+    return response.send("Olá NLW");
+});
+
+app.post("/test/post", (request, response) => {
+    return response.send("Olá NLW método POST");
+});
+
 // http://localhost:3000
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3000, () => console.log("Server is running NLW"));
