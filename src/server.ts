4083192e97dcb11b29f9e69553bc957a20816e7d
wagnersilvafilho <wagnersf97@gmail.com>
2021-06-23 17:48:14 -0300

    Criado entidade tag com service, controller e melhorias do middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, response, Response } from "express";
+import "express-async-errors"
 
 import { router } from "./routes";
 
@@ -10,5 +11,21 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+//middleware de erro possui 4 parametros
+//biblioteca do express não tem suporte async
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        });
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    });
+});
 
 app.listen(3000, () => console.log("Server is running NLW"));
